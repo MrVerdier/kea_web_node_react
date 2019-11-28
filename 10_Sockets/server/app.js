186541c7d9c4ac6c0f1a68bfa4e800f68687f14e
MrVerdier <ande5630@stud.kea.dk>
2019-11-28 14:20:32 +0100

    addded sockets

--- 10_Sockets/server/app.js
+++ 10_Sockets/server/app.js
@@ -1,8 +1,26 @@
-const socket = require('socket.io')
 const express = require('express')
 const app = express()
 
 const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+
+io.on("connection", socket => {
+    // console.log("Socket joined", socket.id)
+    socket.on("I'm thinking about this", data => {
+        // sends out to all clients
+        // io.emit("Someone said", data)
+
+        // only sends to the client itself
+        // socket.emit("Someone said", data)
+
+        // sends to all clients but the client itself
+        socket.broadcast.emit("Someone said", data)
+    })
+    
+    // socket.on("disconnect", () => {
+    //     console.log("Socket left", socket.id)
+    // })
+})
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html")
