f3f27fc5542f0ecf49d93e9f7e9da5c10c7fcc4c
MrVerdier <ande5630@stud.kea.dk>
2019-12-05 10:01:30 +0100

    update

--- 10_Sockets/server/app.js
+++ 10_Sockets/server/app.js
@@ -1,37 +1,43 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-const server = require('http').createServer(app)
-const io = require('socket.io')(server)
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+
+const escape = require("escape-html");
 
 io.on("connection", socket => {
-    // console.log("Socket joined", socket.id)
+    // console.log("Socket joined", socket.id);
+    
     socket.on("I'm thinking about this", data => {
-        // sends out to all clients
-        // io.emit("Someone said", data)
+        // sends out to all the clients
+        io.emit("Someone said", { message: escape(data.message) });
 
         // only sends to the client itself
-        // socket.emit("Someone said", data)
+        // socket.emit("Someone said", data);
 
         // sends to all clients but the client itself
-        socket.broadcast.emit("Someone said", data)
-    })
+        // socket.broadcast.emit("Someone said", data);
+    });
     
     // socket.on("disconnect", () => {
-    //     console.log("Socket left", socket.id)
-    // })
-})
+    //     console.log("Socket left", socket.id);
+    // });
+});
+
+
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html")
-})
+    res.sendFile(__dirname + "/index.html");
+});
 
-const environment = process.env.ENVIRONMENT
-const PORT = environment === "PROD" ? 80 : 8080
+// remove the hardcoded "PROD" and make it not hardcoded
+const environment = process.env.ENVIRONMENT;
+const PORT = environment === "PROD" ? 80 : 8080;
 
-server.listen(PORT, (error) =>{
+server.listen(PORT, (error) => {
     if (error) {
-        console.log(error)
+        console.log(error);
     }
-    console.log("Server is running on port", PORT)
-})
+    console.log("Server is running on Port:", PORT);
+});
