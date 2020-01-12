d482068cac3fbac555374bcb423bc23cb3d9615a
MrVerdier <ande5630@stud.kea.dk>
2020-01-12 20:10:46 +0100

    exam3

--- 12_exam/knexfile.js
+++ 12_exam/knexfile.js
@@ -5,9 +5,10 @@ module.exports = {
   development: {
     client: 'mysql2',
     connection: {
-      database: credentials.database,
-      user:     credentials.user,
-      password: credentials.password
+      host : credentioals.host,
+      user : credentials.user,
+      password : credentials.password,
+      database : credentials.database
     },
     
   },
